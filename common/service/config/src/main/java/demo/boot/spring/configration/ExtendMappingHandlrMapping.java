4e1456fdce6405ff4fbd91d24494d1cba67539f1
guwei <guwei999@126.com>
2019-07-28 15:05:54 +0800

    ignore test

--- common/service/config/src/main/java/demo/boot/spring/configration/ExtendMappingHandlrMapping.java
+++
@@ -1,7 +0,0 @@
-package demo.boot.spring.configration;
-
-/**
- * Created by guwei on 2017/7/26.
- */
-public class ExtendMappingHandlrMapping {
-}
--- common/service/config/src/main/java/demo/boot/spring/web/support/handler/json/support/JsonResultEnum.java
+++
@@ -1,7 +0,0 @@
-package demo.boot.spring.web.support.handler.json.support;
-
-/**
- * Created by guwei on 2017/7/27.
- */
-public class JsonResultEnum {
-}
